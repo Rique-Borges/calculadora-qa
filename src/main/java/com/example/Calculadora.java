390ede0a9cc085f5fc256308d3969919a7f436e1
Rique-Borges <henriqueribeiroborges@gmail.com>
2024-03-25 21:39:44 -0300

    feat add classe Calculadora.java com as 4 operações básicas

--- src/main/java/com/example/Calculadora.java
+++ src/main/java/com/example/Calculadora.java
@@ -4,11 +4,8 @@ public class Calculadora {
 
     /*
      * Calcula a adição de dois números.
-     * 
      * @param a O primeiro número a ser adicionado.
-     * 
      * @param b O segundo número a ser adicionado.
-     * 
      * @return A soma de a e b.
      */
     public double soma(double a, double b) {
@@ -17,11 +14,8 @@ public class Calculadora {
 
     /*
      * Calcula a subtração de dois números.
-     * 
      * @param a O primeiro número a ser subtraído.
-     * 
      * @param b O segundo número a ser subtraído.
-     * 
      * @return A subtração de a e b.
      */
     public double subtrai(double a, double b) {
@@ -30,11 +24,8 @@ public class Calculadora {
 
     /*
      * Calcula a multiplicação de dois números.
-     * 
      * @param a O primeiro número a ser multiplicado.
-     * 
      * @param b O segundo número a ser multiplicado.
-     * 
      * @return A multiplicação de a e b.
      */
     public double multiplica(double a, double b) {
@@ -43,13 +34,9 @@ public class Calculadora {
 
     /*
      * Calcula a divisão de dois números.
-     * 
      * @param a O primeiro número a ser dividido.
-     * 
      * @param b O segundo número a ser dividido.
-     * 
      * @return A divisão de a e b.
-     * 
      * @throws ArithmeticException se b for zero.
      */
     public double divide(double a, double b) {
